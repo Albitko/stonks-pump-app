4ba9afa281c0d13674c6f69a67afed84f6361244
Albitko <alxsmrn@yandex.ru>
2021-08-17 20:52:08 +0300

    Creating template of the app

---
+++ StonksPump/StonksPump/StonksPumpApp.swift
@@ -0,0 +1,17 @@
+//
+//  StonksPumpApp.swift
+//  StonksPump
+//
+//  Created by Alex on 17.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct StonksPumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

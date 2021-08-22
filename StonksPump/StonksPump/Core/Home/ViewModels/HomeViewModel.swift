e0140f75b104d88b3e6d7978b9e49b7ec6052e09
Albitko <alxsmrn@yandex.ru>
2021-08-22 18:52:24 +0300

    Add filtering coins mechanism

--- StonksPump/StonksPump/Core/Home/ViewModels/HomeViewModel.swift
+++ StonksPump/StonksPump/Core/Home/ViewModels/HomeViewModel.swift
@@ -21,10 +21,29 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
-        dataService.$allCoins
+
+        // Updates allCoins
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main )
+            .map(filterCoins)
             .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
             }
-            .store(in: &cancelable )
+            .store(in: &cancelable)
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else {
+             return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        
+        return coins.filter { coin -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+                coin.symbol.contains(lowercasedText) ||
+                coin.id.contains(lowercasedText)
+        }
     }
 }

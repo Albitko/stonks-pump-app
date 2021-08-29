f475e7193c5700147a8b499272992362e06da0da
Albitko <alxsmrn@yandex.ru>
2021-08-29 12:12:15 +0300

    Add input frame for value of coins in portfolio

--- StonksPump/StonksPump/Core/Home/Views/PortfolioView.swift
+++ StonksPump/StonksPump/Core/Home/Views/PortfolioView.swift
@@ -11,32 +11,12 @@ struct PortfolioView: View {
     
     @EnvironmentObject private var vm: HomeViewModel
     @State private var selectedCoin: CoinModel? = nil
+    @State private var quantityText = ""
         
     var body: some View {
         NavigationView{
             ScrollView {
-                VStack(alignment: .leading, spacing: 0) {
-                    SearchBarView(searchText: $vm.searchText)
-                    
-                    ScrollView(.horizontal, showsIndicators: false, content: {
-                        LazyHStack(spacing: 10){
-                            ForEach(vm.allCoins) { coin in
-                                CoinLogoView(coin: coin)
-                                    .frame(width: 75)
-                                    .padding(4)
-                                    .onTapGesture {
-                                        withAnimation(.easeIn){
-                                            selectedCoin = coin
-                                        }
-                                    }
-                                    .background(
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .stroke(selectedCoin?.id == coin.id ? Color.theme.green : Color.clear, lineWidth: 1)
-                                    )
-                            }
-                        }
-                    })
-                }
+                portfolioInputSection
             }
             .navigationTitle("Edit portfolio")
             .toolbar(content: {
@@ -54,5 +34,71 @@ struct PortfolioView_Previews: PreviewProvider {
     static var previews: some View {
         PortfolioView()
             .environmentObject(dev.homeVM)
+    }
+}
+
+extension PortfolioView {
+    private var coinLogoList: some View {
+        ScrollView(.horizontal, showsIndicators: false, content: {
+            LazyHStack(spacing: 10){
+                ForEach(vm.allCoins) { coin in
+                    CoinLogoView(coin: coin)
+                        .frame(width: 75)
+                        .padding(4)
+                        .onTapGesture {
+                            withAnimation(.easeIn){
+                                selectedCoin = coin
+                            }
+                        }
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(selectedCoin?.id == coin.id ? Color.theme.green : Color.clear, lineWidth: 1)
+                        )
+                }
+            }
+            .frame(height: 120)
+            .padding(.leading)
+        })
+    }
+    
+    private func getCurrentValue() -> Double {
+        if let quantity = Double(quantityText){
+            return quantity * (selectedCoin?.currentPrice ?? 0)
+        }
+        return 0
+    }
+    
+    private var portfolioInputSection: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            SearchBarView(searchText: $vm.searchText)
+            coinLogoList
+            
+            if selectedCoin != nil {
+                VStack(spacing: 20){
+                    HStack {
+                        Text("Current price of \(selectedCoin?.symbol.uppercased() ?? ""):")
+                        Spacer()
+                        Text(selectedCoin?.currentPrice.asCurencyWith2Decimials() ?? "")
+                    }
+                    Divider()
+                    HStack {
+                        Text("Amount in your portfolio:")
+                        Spacer()
+                        TextField("Ex. 1.4", text: $quantityText)
+                            .multilineTextAlignment(.trailing)
+                            .keyboardType(.decimalPad)
+                    }
+                    Divider()
+                    HStack {
+                        Text("Current value:")
+                        Spacer()
+                        Text(getCurrentValue().asCurencyWith2Decimials())
+                    }
+                }
+                .animation(.none)
+                .padding()
+                .font(.headline)
+            }
+        }
     }
 }

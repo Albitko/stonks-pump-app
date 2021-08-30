de1ae6ed2f5252b02127f7cb189c435139a66a98
Albitko <alxsmrn@yandex.ru>
2021-08-30 13:36:00 +0300

    Add storing portfolio view data in core data

--- StonksPump/StonksPump/Core/Home/ViewModels/HomeViewModel.swift
+++ StonksPump/StonksPump/Core/Home/ViewModels/HomeViewModel.swift
@@ -18,6 +18,7 @@ class HomeViewModel: ObservableObject {
     
     private let coinDataService = CoinDataService()
     private let marketDataService = MarketDataService()
+    private let portfolioDataService = PortfoiloDataService()
     private var cancelable = Set<AnyCancellable>()
     
     init() {
@@ -44,6 +45,25 @@ class HomeViewModel: ObservableObject {
                 self?.statistics = returnedStats
             }
             .store(in: &cancelable)
+        
+        // Update portfolioCoins
+        $allCoins
+            .combineLatest(portfolioDataService.$savedEntities)
+            .map { coinModels, portfolioEnities -> [CoinModel] in
+                coinModels
+                    .compactMap { coin -> CoinModel? in
+                        guard let entity = portfolioEnities.first(where: { $0.coinID == coin.id}) else { return nil }
+                        return coin.updateHoldings(amount: entity.amount)
+                    }
+            }
+            .sink { [weak self] returnedCoins in
+                self?.portfolioCoins = returnedCoins
+            }
+            .store(in: &cancelable)
+    }
+    
+    func updatePortfolio(coin: CoinModel, amount: Double) {
+        portfolioDataService.updatePortfolio(coin: coin, amount: amount)
     }
     
     private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
--- StonksPump/StonksPump/Core/Home/Views/PortfolioView.swift
+++ StonksPump/StonksPump/Core/Home/Views/PortfolioView.swift
@@ -37,6 +37,11 @@ struct PortfolioView: View {
                     trailingNavBarItems
                 }
             })
+            .onChange(of: vm.searchText, perform: { value in
+                if value == "" {
+                    removeSelectedCoin()
+                }
+            })
         }
     }
 }
@@ -52,7 +57,7 @@ extension PortfolioView {
     private var coinLogoList: some View {
         ScrollView(.horizontal, showsIndicators: false, content: {
             LazyHStack(spacing: 10){
-                ForEach(vm.allCoins) { coin in
+                ForEach(vm.searchText.isEmpty ? vm.portfolioCoins : vm.allCoins ) { coin in
                     CoinLogoView(coin: coin)
                         .frame(width: 75)
                         .padding(4)
@@ -70,6 +75,17 @@ extension PortfolioView {
             .frame(height: 120)
             .padding(.leading)
         })
+    }
+    
+    private func updateSelectedCoin(coin: CoinModel) {
+        selectedCoin = coin
+        
+        if let portfolioCoin = vm.portfolioCoins.first(where: {$0.id == coin.id}),
+           let amount = portfolioCoin.currentHoldings {
+            quantityText = "\(amount)"
+        }else {
+            quantityText = ""
+        }
     }
     
     private func getCurrentValue() -> Double {
@@ -123,9 +139,13 @@ extension PortfolioView {
     }
     
     private func saveButtonPressed() {
-        guard let coin = selectedCoin else { return }
+        guard
+            let coin = selectedCoin,
+            let amount = Double(quantityText)
+        else { return }
         
         // Save to portfolio
+        vm.updatePortfolio(coin: coin , amount: amount)
         
         // Show checkmark
         withAnimation(.easeIn) {
